22713a5062897998e4bbc0ed42dc8c4cb491f7f5
mbdev0 <mustafababar7@gmail.com>
2022-06-29 03:43:46 +0100

    fixed folder structure

---
+++ learning_scripts/day-1/datatypes.js
@@ -0,0 +1,4 @@
+console.log(typeof 'String')
+console.log(typeof true)
+console.log(typeof 20)
+console.log(typeof null)
---
+++ learning_scripts/day-1/variable.js
@@ -0,0 +1,7 @@
+let github_name = 'mbdev0'
+let isDeveloper = true
+let age
+let recursion_skills = null
+
+let number = 43, cat_name = 'Sushi', dog_name = 'Charlie'
+console.log(number,cat_name,dog_name)
